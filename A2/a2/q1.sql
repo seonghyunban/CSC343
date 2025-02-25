8bc7423fb73f29f359d450ff5be2430305045b50
seonghyunban <seonghyun.ban@pm.me>
2025-02-25 18:10:19 -0500

    part1 q1 done

--- A2/a2/q1.sql
+++ A2/a2/q1.sql
@@ -18,10 +18,52 @@ CREATE TABLE q1 (
 -- (But give them better names!) The IF EXISTS avoids generating an error
 -- the first time this file is imported.
 -- If you do not define any views, you can delete the lines about views.
-DROP VIEW IF EXISTS intermediate_step CASCADE;
+DROP VIEW IF EXISTS PlaneInfo CASCADE;
+DROP VIEW IF EXISTS FlightBookInfo CASCADE;
+DROP VIEW IF EXISTS FlightBookNRouteNFullnessInfo CASCADE;
+DROP VIEW IF EXISTS FullnessHistogram CASCADE;
+
+
 
 -- Define views for your intermediate steps here:
-CREATE VIEW intermediate_step AS ... ;
+
+CREATE VIEW PlaneInfo AS (
+	SELECT tail_number as pid, count(S.sid) as cap
+	FROM Plane P, Seat S
+	WHERE P.tail_number = S.plane
+	GROUP BY tail_number
+);
+
+
+CREATE VIEW FlightBookInfo AS (
+	SELECT F.fid, F.route, F.plane, (D.fid is NOT NULL) as departed, count(B.bid) as num_booked
+	FROM Flight F 
+		LEFT JOIN Booking B ON F.fid = B.flight
+		LEFT JOIN Departure D ON F.fid = D.fid
+	GROUP BY F.fid, D.fid
+);
+
+
+CREATE VIEW FlightBookNRouteNFullnessInfo AS (
+	SELECT F.fid, F.route, F.departed, F.plane, R.airline, F.num_booked, P.cap, (F.num_booked::FLOAT / P.cap) as fullness
+	FROM FlightBookInfo F 
+		JOIN Route R ON F.route = R.flight_num
+		JOIN PlaneInfo P ON F.plane = P.pid
+);
+
+
+CREATE VIEW FullnessHistogram AS (
+	SELECT airline, route as flight_num,
+		count(*) FILTER (WHERE fullness < 0.2) as very_low,
+		count(*) FILTER (WHERE fullness >= 0.2 AND fullness < 0.4) as low,
+		count(*) FILTER (WHERE fullness >= 0.4 AND fullness < 0.6) as fair,
+		count(*) FILTER (WHERE fullness >= 0.6 AND fullness < 0.8) as normal,
+		count(*) FILTER (WHERE fullness >= 0.8) as high
+	FROM FlightBookNRouteNFullnessInfo F
+	GROUP BY route, airline
+);
+
 
 -- Your query that answers the question goes below the "insert into" line:
 INSERT INTO q1
+	SELECT * FROM FullnessHistogram
